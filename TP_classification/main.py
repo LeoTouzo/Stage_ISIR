b92bf466bb682be9aecfa35a7aa979a4097c2b3d
leo.touzo <leo.touzo@gmail.com>
2021-10-26 15:26:32 +0200

    Finished TP classification

--- TP_classification/main.py
+++ TP_classification/main.py
@@ -76,28 +76,101 @@ class Net(nn.Module):
         return x
 
 
-data = loadImgs(batch_size_train=batch_size_train, batch_size_test=batch_size_test)
+# data = loadImgs(batch_size_train=batch_size_train, batch_size_test=batch_size_test)
 
 net = Net().to(device)
-optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
-
-# net.load_state_dict(torch.load('./data/model_TP.pt'))
-
-num_epochs = 100
-epochs = np.arange(num_epochs+1)
-accuracy = [evaluate(net, data)]
-for j in range(num_epochs):
-    print(f"epoch {j} / {num_epochs}")
-    train_epoch(net, optimizer, data)
-    accuracy.append(evaluate(net, data))
-    torch.save(net.state_dict(), './data/model_TP.pt')
-
-# plt.plot(epochs,accuracy)
-# plt.xlabel('epoch')
-# plt.ylabel('accuracy')
-np.savetxt('.data/accuracy.txt',accuracy)
-
-indices = np.random.choice(range(data.num_test_samples), 20)
-
+# optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+#
+net.load_state_dict(torch.load('./data/model_TP.pt'))
+net.eval()
+#
+# num_epochs = 100
+# epochs = np.arange(num_epochs+1)
+# accuracy = [evaluate(net, data)]
+# for j in range(num_epochs):
+#     print(f"epoch {j} / {num_epochs}")
+#     train_epoch(net, optimizer, data)
+#     accuracy.append(evaluate(net, data))
+#     torch.save(net.state_dict(), './data/model_TP.pt')
+#
+# # plt.plot(epochs,accuracy)
+# # plt.xlabel('epoch')
+# # plt.ylabel('accuracy')
+# np.savetxt('.data/accuracy.txt',accuracy)
+#
+# indices = np.random.choice(range(data.num_test_samples), 20)
+# #
 # plotdata(data, indices, net, original=False)
 
+# evaluate(net,data)
+
+def several_crops(model, n_crops, des_dir="./data/", img_size=100, batch_size_test=100):
+    model = model.eval()
+    oupt = []
+    for i in range(n_crops):
+        dataset_test = dset.ImageFolder(root=des_dir + "test/",
+                                        transform=transforms.Compose([
+                                            transforms.Resize(img_size),
+                                            transforms.RandomCrop(75, padding=4),
+                                            transforms.RandomHorizontalFlip(),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                                        ]))
+        dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=batch_size_test, shuffle=False)
+        batch_enum = enumerate(dataloader_test)
+        batch_idx, (testdata, testtargets) = next(batch_enum)
+        oupt.append(model(testdata))
+    avg_oupt = torch.argmax(sum(oupt), dim=1)
+    t = torch.sum(avg_oupt == testtargets)
+    result = t * 100.0 / len(testtargets)
+    model = model.train()
+    print(f"{t} correct on {len(testtargets)} ({result.item()} %)")
+    return result.item()
+
+# several_crops(net, 5, batch_size_test=batch_size_test)
+
+def worst(n, indices, model, data):
+    model = model.eval()
+    testdata_full = data.test
+    testdata = torch.stack([testdata_full[i][0] for i in indices])
+    testtargets = torch.tensor([testdata_full[i][1] for i in indices])
+    oupt = model(testdata)
+    pwrong = torch.exp(oupt[torch.arange(oupt.size(0)),1-testtargets])/torch.sum(torch.exp(oupt),dim=1)
+    argworst = torch.topk(pwrong, n)
+    print(testtargets[argworst.indices])
+    plt.figure()
+    plotdata(data, argworst.indices, net, original=False)
+    return argworst
+
+# worst(5, np.arange(200), net, data)
+
+x = torch.rand((3,75,75))*1e-1
+x.requires_grad = True
+for param in net.parameters():
+    param.requires_grad = False
+
+lr = 0.01
+num_epochs = 100000
+loss = nn.Softmax()(net(x)[0])[1]
+epochs = np.arange(num_epochs+1)
+loss_list = [loss.item()]
+for j in range(num_epochs):
+    loss.backward()
+    with torch.no_grad():
+        x -= lr*x.grad
+        x.grad.zero_()
+    loss = nn.Softmax()(net(x)[0])[1]
+    loss_list.append(loss.item())
+    if j%1000==0:
+        print(j)
+
+plt.plot(epochs,loss_list)
+np.savetxt('.data/young_loss.txt',loss_list)
+plt.xlabel('epoch')
+plt.ylabel('P(class=0)')
+plt.figure()
+print(nn.Softmax()(net(x)))
+im = np.transpose(x.detach().numpy(),(1,2,0))
+np.savetxt('./data/young.txt',im)
+plt.imshow(im)
+plt.show()
